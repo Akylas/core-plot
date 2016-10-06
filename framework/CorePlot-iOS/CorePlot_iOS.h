edf0ccc128adf1443b5b7aa79074bab552e7b025
Martin Guillon <martin.guillon@akylas.fr>
2016-10-06 06:50:24 +0200

    missing

---
+++ framework/CorePlot-iOS/CorePlot_iOS.h
@@ -0,0 +1,13 @@
+//
+//  CorePlot_iOS.h
+//  CorePlot-iOS
+//
+//  Created by Martin Guillon on 07/07/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CorePlot_iOS : NSObject
+
+@end
---
+++ framework/CorePlot-iOS/CorePlot_iOS.m
@@ -0,0 +1,13 @@
+//
+//  CorePlot_iOS.m
+//  CorePlot-iOS
+//
+//  Created by Martin Guillon on 07/07/16.
+//
+//
+
+#import "CorePlot_iOS.h"
+
+@implementation CorePlot_iOS
+
+@end
